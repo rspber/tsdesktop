2fd1e8af5e1bc953ed3bfddbf0170ada01c9081b
rspber <kopropa@onet.pl>
2022-11-05 19:49:26 +0100

    scroller: clearer local variable names

--- tsdesktop/Scroller.cpp
+++ tsdesktop/Scroller.cpp
@@ -73,49 +73,49 @@ void Container::drawScroller()
   int8_t hs = 16;
   clip_t clip;
   getOuterClip(clip);
-  int16_t w = clip.x2 - clip.x1;
-  int16_t h = clip.y2 - clip.y1;
+  int16_t page_width = clip.x2 - clip.x1;
+  int16_t page_height = clip.y2 - clip.y1;
   int8_t mgr = 0;
   if (clip.y2 < y + updHeight) {
-    int16_t x1 = x + w - ws;
+    int16_t x1 = x + page_width - ws;
     int16_t y1 = y;
     draw_btn(x1, y1 += 0, SCRLBMPT[6]);
     draw_btn(x1, y1 += hs + 2, SCRLBMPT[7]);
     draw_btn(x1, y1 += hs + 2, SCRLBMPT[8]);
     y1 += hs + 2;
 
-    int16_t yt = y1;
+    int16_t y_progress_top = y1;
 
-    y1 = y + h;
+    y1 = y + page_height;
     draw_btn(x1, y1 -= hs, SCRLBMPT[11]);
     draw_btn(x1, y1 -= hs + 2, SCRLBMPT[10]);
     draw_btn(x1, y1 -= hs + 2, SCRLBMPT[ 9]);
 
     if (scroll_progress) {
-      int8_t percent = offsetTop * 100 / (updHeight - h);
-      draw_progress(x1, yt + (percent * (y1 - yt - 10) / 100 ) - 4, SCRLBMPT[12] );
+      int8_t percent = offsetTop * 100 / (updHeight - page_height);
+      draw_progress(x1, y_progress_top + (percent * (y1 - y_progress_top - 10) / 100 ) - 4, SCRLBMPT[12] );
     }
 
     mgr = 1;
   }
   if (clip.x2 < x + updWidth) {
-    int16_t y1 = y + h - hs;
+    int16_t y1 = y + page_height - hs;
     int16_t x1 = x;
     draw_btn(x1 += 0, y1, SCRLBMPT[0]);
     draw_btn(x1 += ws + 2, y1, SCRLBMPT[1]);
     draw_btn(x1 += ws + 2, y1, SCRLBMPT[2]);
     x1 += ws + 2;
 
-    int16_t xl = x1;
+    int16_t x_progress_left = x1;
 
-    x1 = x + w;
+    x1 = x + page_width;
     draw_btn(x1 -= ws + mgr * (16 + 2), y1, SCRLBMPT[5]);
     draw_btn(x1 -= ws + 2, y1, SCRLBMPT[4]);
     draw_btn(x1 -= ws + 2, y1, SCRLBMPT[3]);
 
     if (scroll_progress) {
-      int8_t percent = offsetLeft * 100 / (updWidth - w);
-      draw_progress(xl + (percent * (x1 - xl - 10) / 100) - 4, y1, SCRLBMPT[12]);
+      int8_t percent = offsetLeft * 100 / (updWidth - page_width);
+      draw_progress(x_progress_left + (percent * (x1 - x_progress_left - 10) / 100) - 4, y1, SCRLBMPT[12]);
     }
   }
 }
@@ -211,11 +211,11 @@ const bool Container::scrollerPressed(const int16_t xScreen, const int16_t yScre
   int8_t hs = 16;
   clip_t clip;
   getOuterClip(clip);
-  int16_t w = clip.x2 - clip.x1;
-  int16_t h = clip.y2 - clip.y1;
+  int16_t page_width = clip.x2 - clip.x1;
+  int16_t page_height = clip.y2 - clip.y1;
   int8_t mgr = 0;
   if (clip.y2 < y + updHeight) {
-    int16_t x1 = x + w - ws;
+    int16_t x1 = x + page_width - ws;
     int16_t y1 = y;
     if (btn_pressed(x1, y1 += 0, xScreen, yScreen)) {
       scroll(SCROLL_HOME, true, true);
@@ -230,7 +230,7 @@ const bool Container::scrollerPressed(const int16_t xScreen, const int16_t yScre
       return true;
     }
 
-    y1 = y + h;
+    y1 = y + page_height;
     if (btn_pressed(x1, y1 -= hs, xScreen, yScreen)) {
       scroll(SCROLL_HOME, false, true);
       return true;
@@ -246,7 +246,7 @@ const bool Container::scrollerPressed(const int16_t xScreen, const int16_t yScre
     mgr = 1;
   }
   if (clip.x2 < x + updWidth) {
-    int16_t y1 = y + h - hs;
+    int16_t y1 = y + page_height - hs;
     int16_t x1 = x;
     if (btn_pressed(x1 += 0, y1, xScreen, yScreen)) {
       scroll(SCROLL_HOME, true, false);
@@ -261,7 +261,7 @@ const bool Container::scrollerPressed(const int16_t xScreen, const int16_t yScre
       return true;
     }
 
-    x1 = x + w;
+    x1 = x + page_width;
     if (btn_pressed(x1 -= ws + mgr * (16 + 2), y1, xScreen, yScreen)) {
       scroll(SCROLL_HOME, false, false);
       return true;
