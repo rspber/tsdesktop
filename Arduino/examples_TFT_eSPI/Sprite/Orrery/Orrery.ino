301d5c819b87e3bed8f45d9974b5b03a9a13cc8c
rspber <kopropa@onet.pl>
2024-10-24 15:17:59 +0200

    TFT_eSPI running examples

---
+++ Arduino/examples_TFT_eSPI/Sprite/Orrery/Orrery.ino
@@ -0,0 +1,165 @@
+// Display an Orrery
+// Works for all display sizes but 320x480 minimum size recommended
+// Whole planet orbits only visible in 480 x 800 display
+
+// Flicker free sprite example for TFT_eSPI:
+// https://github.com/Bodmer/TFT_eSPI
+// Sketch coded by Bodmer
+// Uses astronomy engine created by Don Cross
+
+#include <TFT_eSPI.h>                 // Hardware-specific library
+
+TFT_eSPI    tft;         // Invoke library
+
+TFT_eSprite img;  // Sprite class
+
+#define sunX tft.width()/2
+#define sunY tft.height()/2
+
+uint16_t orb_inc;
+uint16_t planet_r;
+
+#include <stdio.h>
+#include "astronomy.h"
+#define TIME_TEXT_BYTES  25
+
+astro_time_t astro_time;
+
+#define TRANSPARENT DARK_GREEN
+
+rgb_t grey;
+
+static const astro_body_t body[] = {
+  BODY_SUN, BODY_MERCURY, BODY_VENUS, BODY_EARTH, BODY_MARS,
+  BODY_JUPITER, BODY_SATURN, BODY_URANUS, BODY_NEPTUNE
+};
+
+static const uint32_t bodyColour[] = {
+  YELLOW, DARKGREY, ORANGE, BLUE, RED,
+  GOLDEN, BROWN, DARK_CYAN, CYAN
+};
+
+
+// =========================================================================
+// Setup
+// =========================================================================
+void setup() {
+  Serial.begin(115200);
+  tft.begin();
+  tft.setRotation(1);
+  tft.fillScreen(BLACK);
+
+  // Test with smaller display sizes
+  //tft.setViewport(10,10,160,128);
+  //tft.setViewport(10,10,320,240);
+  //tft.setViewport(10,10,480,320);
+  //tft.frameViewport(TFT_GREEN, -1);
+
+  img.createSprite(19, 19);
+
+  grey = RGB(30, 30, 30);
+
+  astro_time = Astronomy_MakeTime(2020, 10, 16, 19, 31, 0) ;
+  tft.fillCircle(sunX, sunY, 10, YELLOW);
+
+  // i initialised to 1 so Sun is skipped
+  for (int i = 1; i < sizeof(body) / sizeof(body[0]); ++i)
+  {
+    tft.drawCircle(sunX, sunY, i * 28, grey);
+  }
+}
+
+
+// =========================================================================
+// Loop
+// =========================================================================
+void loop() {
+  uint32_t dt = millis();
+  plot_planets();
+  showTime(astro_time);
+
+  // Add time increment (more than 0.6 days will lead to stray pixel on screen
+  // due to the way previous object images are erased)
+  astro_time = Astronomy_AddDays(astro_time, 0.25); // 0.25 day (6 hour) increment
+
+  dt = millis()-dt;
+  //Serial.println(dt);
+  //delay(1000);
+}
+
+// =========================================================================
+// Get coordinates of end of a vector, pivot at x,y, length r, angle a
+// =========================================================================
+// Coordinates are returned to caller via the xp and yp pointers
+#define DEG2RAD 0.0174532925
+void getCoord(int x, int y, int *xp, int *yp, int r, float a)
+{
+  float sx1 = cos( -a * DEG2RAD );
+  float sy1 = sin( -a * DEG2RAD );
+  *xp =  sx1 * r + x;
+  *yp =  sy1 * r + y;
+}
+
+// =========================================================================
+// Convert astronomical time to UTC and display
+// =========================================================================
+void showTime(astro_time_t time)
+{
+    astro_status_t status;
+    char text[TIME_TEXT_BYTES];
+
+    status = Astronomy_FormatTime(time, TIME_FORMAT_SECOND, text, sizeof(text));
+    if (status != ASTRO_SUCCESS)
+    {
+        fprintf(stderr, "\nFATAL(PrintTime): status %d\n", status);
+        exit(1);
+    }
+    
+    tft.drawText(0, 0, text, 2);
+}
+
+// =========================================================================
+// Plot planet positions as an Orrery
+// =========================================================================
+int plot_planets(void)
+{
+  astro_angle_result_t ang;
+
+  int i;
+  int num_bodies = sizeof(body) / sizeof(body[0]);
+
+  // i initialised to 1 so Sun is skipped
+  for (i = 1; i < num_bodies; ++i)
+  {
+    ang = Astronomy_EclipticLongitude(body[i], astro_time);
+
+    int x1 = 0; // getCoord() will update these
+    int y1 = 0;
+
+    getCoord(0, 0, &x1, &y1, i * 28, ang.angle); // Get x1 ,y1
+
+    img.fillSprite(TRANSPARENT);
+    img.fillCircle(9, 9, 9, BLACK);
+    img.drawCircle(9 - x1, 9 - y1, i * 28, grey);
+    img.fillCircle(9, 9, 5, bodyColour[i]);
+    img.pushSprite(tft, sunX + x1 - 9, sunY + y1 - 9, TRANSPARENT);
+
+    if (body[i] == BODY_EARTH)
+    {
+      astro_angle_result_t mang = Astronomy_LongitudeFromSun(BODY_MOON, astro_time);
+
+      int xm = 0;
+      int ym = 0;
+
+      getCoord(x1, y1, &xm, &ym, 15, 180 + ang.angle + mang.angle); // Get x1 ,y1
+
+      img.fillSprite(TRANSPARENT);
+      img.fillCircle(9, 9, 7, BLACK);
+      img.drawCircle(9 - xm, 9 - ym, i * 28, grey);
+      img.fillCircle(9, 9, 2, WHITE);
+      img.pushSprite(tft, sunX + xm - 9, sunY + ym - 9, TRANSPARENT);
+    }
+  }
+
+  return 0;
+}
---
+++ Arduino/examples_TFT_eSPI/Sprite/Rotated_Sprite_1/Rotated_Sprite_1.ino
@@ -0,0 +1,190 @@
+// This example plots a rotated Sprite to the screen using the pushRotated()
+// function. It is written for a 240 x 320 TFT screen.
+
+// Two rotation pivot points must be set, one for the Sprite and one for the TFT
+// using setPivot(). These pivot points do not need to be within the visible screen
+// or Sprite boundary.
+
+// When the Sprite is rotated and pushed to the TFT with pushRotated(angle) it will be
+// drawn so that the two pivot points coincide. This makes rotation about a point on the
+// screen very simple. The rotation is clockwise with increasing angle. The angle is in
+// degrees, an angle of 0 means no Sprite rotation.
+
+// The pushRotated() function works with 1, 4, 8 and 16 bit per pixel (bpp) Sprites.
+
+// The original Sprite is unchanged so can be plotted again at a different angle.
+
+// Optionally a transparent colour can be defined, pixels of this colour will
+// not be plotted to the TFT.
+
+// For 1 bpp Sprites the foreground and background colours are defined with the
+// function spr.setBitmapColor(foregroundColor, backgroundColor).
+
+// For 4 bpp Sprites the colour map index is used instead of the 16 bit colour
+// e.g. spr.setTextColor(5); // Green text in default colour map
+// See "Transparent_Sprite_Demo_4bit" example for default colour map details
+
+// Created by Bodmer 6/1/19 as an example to the TFT_eSPI library:
+// https://github.com/Bodmer/TFT_eSPI
+
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft;           // TFT object
+
+TFT_eSprite spr; // Sprite object
+
+// =======================================================================================
+// Setup
+// =======================================================================================
+
+void setup()   {
+  Serial.begin(9600); // Debug only
+
+  tft.begin();  // initialize
+  tft.setRotation(0);
+}
+
+// =======================================================================================
+// Loop
+// =======================================================================================
+
+void loop() {
+  
+  int xw = tft.width()/2;   // xw, yh is middle of screen
+  int yh = tft.height()/2;
+
+
+  showMessage("90 degree angles");
+  tft.setPivot(xw, yh);     // Set pivot to middle of TFT screen
+  drawX(xw, yh);            // Show where screen pivot is
+
+  // Create the Sprite
+  spr.setColorDepth(8);      // Create an 8bpp Sprite of 60x30 pixels
+  spr.createSprite(64, 30);  // 8bpp requires 64 * 30 = 1920 bytes
+  spr.setPivot(32, 55);      // Set pivot relative to top left corner of Sprite
+  spr.fillSprite(BLACK); // Fill the Sprite with black
+
+//  spr.setTextColor(TFT_GREEN);        // Green text
+//  spr.setTextDatum(MC_DATUM);         // Middle centre datum
+  spr.drawText(2, 5, "Hello", 2, YELLOW); // Plot text, font 4, in Sprite at 30, 15
+
+  spr.pushRotated(tft, 0);
+  spr.pushRotated(tft, 90);
+  spr.pushRotated(tft, 180);
+  spr.pushRotated(tft, 270);
+
+  delay(2000);
+
+
+  showMessage("45 degree angles");
+  drawX(xw, yh);            // Show where screen pivot is
+
+  spr.pushRotated(tft, 45);
+  spr.pushRotated(tft, 135);
+  spr.pushRotated(tft, 225);
+  spr.pushRotated(tft, 315);
+
+  delay(2000);  // Pause so we see it
+
+
+  showMessage("Moved Sprite pivot point");
+  drawX(xw, yh);          // Show where screen pivot is
+
+  spr.setPivot(-20, 15);  // Change just the Sprite pivot point
+
+  spr.pushRotated(tft, 45);
+  spr.pushRotated(tft, 135);
+  spr.pushRotated(tft, 225);
+  spr.pushRotated(tft, 315);
+
+  delay(2000);  // Pause so we see it
+
+
+  showMessage("Moved TFT pivot point");
+  tft.setPivot(100, 100); // Change just the TFT pivot point
+  drawX(100, 100);        // Show where pivot is
+
+  spr.pushRotated(tft, 45);
+  spr.pushRotated(tft, 135);
+  spr.pushRotated(tft, 225);
+  spr.pushRotated(tft, 315);
+
+  delay(2000);  // Pause so we see it
+
+
+  showMessage("Transparent rotations");
+  tft.fillCircle(xw, yh, 70, GRAY); // Draw a filled circle
+
+  tft.setPivot(xw, yh);         // Set pivot to middle of screen
+  drawX(xw, yh);                // Show where pivot is
+  
+  spr.deleteSprite();
+
+  spr.setColorDepth(8);         // Create a 8bpp Sprite
+  spr.createSprite(40, 30);     // Create a new Sprite 40x30
+  spr.setPivot(20, 70);         // Set Sprite pivot at 20,80
+
+//  spr.setTextColor(RED);    // Red text in Sprite
+//  spr.setTextDatum(MC_DATUM);   // Middle centre datum
+
+  int num = 1;
+
+  for (int16_t angle = 30; angle <= 360; angle += 30)
+  {
+    spr.fillSprite(BLACK);         // Clear the Sprite
+    char buf[40];
+    snprintf(buf, 40, "%d", num);
+    spr.drawText(10, 10, buf, 2, RED);    // Plot number, in Sprite at 20,15 and with font 4
+    spr.pushRotated(tft, angle, BLACK); // Plot rotated Sprite, black being transparent
+    num++;
+  }
+
+//  spr.setTextColor(WHITE);    // White text in Sprite
+  spr.setPivot(-75, 15);          // Set Sprite pivot at -75,15
+
+  for (int16_t angle = -90; angle < 270; angle += 30)
+  {
+    spr.fillSprite(BLACK);           // Clear the Sprite
+    char buf[40];
+    snprintf(buf, 40, "%d", angle+90);
+    spr.drawText(10, 10, buf, 2, WHITE); // Plot number, in Sprite at 20,15 and with font 4
+    spr.pushRotated(tft, angle, BLACK);   // Plot rotated Sprite, black being transparent
+    num++;
+  }
+  
+  delay(8000);  // Pause so we see it
+
+  spr.deleteSprite();
+
+}
+
+// =======================================================================================
+// Draw an X centered on x,y
+// =======================================================================================
+
+void drawX(int x, int y)
+{
+  tft.drawLine(x-5, y-5, x+5, y+5, WHITE);
+  tft.drawLine(x-5, y+5, x+5, y-5, WHITE);
+}
+
+// =======================================================================================
+// Show a message at the top of the screen
+// =======================================================================================
+
+void showMessage(const char* msg)
+{
+  // Clear the screen areas
+  tft.fillRect(0, 0, tft.width(), 20, BLACK);
+  tft.fillRect(0, 20, tft.width(), tft.height()-20, BLUE);
+
+//  uint8_t td = tft.getTextDatum(); // Get current datum
+
+//  tft.setTextDatum(TC_DATUM);      // Set new datum
+
+  tft.drawText(tft.width()/2, 2, msg, 1); // Message in font 2
+
+//  tft.setTextDatum(td); // Restore old datum
+}
+
+// =======================================================================================
